80deb95df8185596be0b95bc82dec808aa04455d
CEGriffin <20ceg4@queensu.ca>
2025-01-07 11:08:12 -0500

    Created script to generate limb length data of Patagonian caracaras

---
+++ dataGenerato.R
@@ -0,0 +1,20 @@
+#Confirm working directory
+getwd()
+
+#Vector of species names
+spec_names<-sample(c("northern_caracara", "crested_caracara", "chimango_caracara", "striated_caracara", "mountain_caracara"), 100, rep=T)
+
+#vector of limb widths
+limb_width_cm<-round(rnorm(100,3,1),2)
+
+#vector of limb lengths
+limb_length_cm<-round(rnorm(100, 25, 5),2)
+
+#vector of observers
+observer<-sample(c("b_sanderson", "n_gaiman", "v_schwab"), 100, rep=T)
+
+#combine vectors into data frame
+limb_dataframe<-data.frame(spec_names, limb_width_cm, limb_length_cm, observer)
+
+#export to a csv file
+write.csv(limb_dataframe, file="./measurements.csv", row.names=F)
