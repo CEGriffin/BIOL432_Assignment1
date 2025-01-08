a326a9d5209f1ca472144eafa2ec26f570bbdffb
CEGriffin <20ceg4@queensu.ca>
2025-01-07 20:45:14 -0500

    Changed comments to make them more comprehensive

--- dataGenerato.R
+++ dataGenerato.R
@@ -1,16 +1,18 @@
 #Confirm working directory
 getwd()
 
-#Vector of species names
+#Create a vector of species names
 spec_names<-sample(c("northern_caracara", "crested_caracara", "chimango_caracara", "striated_caracara", "mountain_caracara"), 100, rep=T)
 
-#vector of limb widths
+#Create a vector of limb widths
+#round to reasonable significant digits atainable
 limb_width_cm<-round(rnorm(100,3,1),2)
 
-#vector of limb lengths
+#Create a vector of limb lengths
+#round to reasonable significant digits atainable
 limb_length_cm<-round(rnorm(100, 25, 5),2)
 
-#vector of observers
+#Create a vector of observers
 observer<-sample(c("b_sanderson", "n_gaiman", "v_schwab"), 100, rep=T)
 
 #combine vectors into data frame
